0ae9d449984ae1e4f7d88c37dca5e70d66ea5755
andy <andy@andydote.co.uk>
2014-05-15 23:37:03 +0100

    query: implement event deserialization

---
+++ Storage.Relational/EventDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Storage.Relational
+{
+	internal class EventDto
+	{
+		public Guid ID { get; set; }
+		public Guid AggregateID { get; set; }
+		public int Order { get; set; }
+		public string Type { get; set; }
+		public string Json { get; set; }
+	}
+}
--- Storage.Relational/Queries/GetCandidateByID.cs
+++ Storage.Relational/Queries/GetCandidateByID.cs
@@ -1,14 +1,19 @@
 ﻿using System;
+using System.Data.Common;
+using System.Linq;
+using Dapper;
 using Domain;
+using Domain.Infrastructure;
+using Newtonsoft.Json;
 
 namespace Storage.Relational.Queries
 {
 	public class GetCandidateByID
 	{
-		private readonly ISession _session;
+		private readonly DbConnection _session;
 		private readonly Guid _id;
 
-		public GetCandidateByID(ISession session, Guid id)
+		public GetCandidateByID(DbConnection session, Guid id)
 		{
 			_session = session;
 			_id = id;
@@ -16,7 +21,18 @@ namespace Storage.Relational.Queries
 
 		public Candidate Execute()
 		{
-			return null;
+			var dtos = _session.Query<EventDto>(
+				"select id, aggregateID, order, type, json from candidates where aggregateID = @id order by order asc",
+				new { id = _id });
+
+			var events = dtos
+				.Select(dto => JsonConvert.DeserializeObject(dto.Json, Type.GetType(dto.Type)))
+				.Cast<DomainEvent>();
+
+			var candidate = new Candidate();
+			((IEventStream)candidate).LoadFromEvents(events);
+			
+			return candidate;
 		}
 	}
 }
