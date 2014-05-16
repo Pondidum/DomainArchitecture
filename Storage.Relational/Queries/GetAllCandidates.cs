0233cf783880ffb5fdf23fe57560e314babdc40e
andy <andy@andydote.co.uk>
2014-05-16 22:22:27 +0100

    storage.relational: add getAllCandidates

---
+++ Storage.Relational/Queries/GetAllCandidates.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using Dapper;
+using Domain;
+using Domain.Infrastructure;
+using Newtonsoft.Json;
+
+namespace Storage.Relational.Queries
+{
+	public class GetAllCandidates
+	{
+		private readonly DbConnection _connection;
+
+		public GetAllCandidates(DbConnection connection)
+		{
+			_connection = connection;
+		}
+
+		public IEnumerable<Candidate> Execute()
+		{
+			var dtos = _connection.Query<EventDto>(
+				"select id, aggregateID, order, type, json from candidates");
+
+			return dtos
+				.Select(dto => JsonConvert.DeserializeObject(dto.Json, Type.GetType(dto.Type)))
+				.Cast<DomainEvent>()
+				.OrderBy(e => e.SequenceID)
+				.GroupBy(e => e.AggregateID)
+				.Select(group =>
+				{
+					var candidate = new Candidate();
+					((IEventStream)candidate).LoadFromEvents(group);
+					return candidate;
+				});
+		}
+	}
+}
